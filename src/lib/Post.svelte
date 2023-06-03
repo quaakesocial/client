32961f7ba639f8fdf9b8221b75f3e26bd564a08d
MrCookie <110797323+errplanedev@users.noreply.github.com>
2023-06-03 07:53:37 -0700

    ids for loves

--- src/lib/Post.svelte
+++ src/lib/Post.svelte
@@ -9,7 +9,7 @@
   };
 
   let api: string;
-  let username: string;
+  let id: string;
   let token: string | null;
   let loggedIn: boolean = true;
 
@@ -45,7 +45,7 @@
 
   async function lovePost() {
     let res;
-    if(post.loves.includes(username)) {
+    if(post.loves.includes(id)) {
       res = await axios.delete(`${api}/p/${post.id}/love`, { headers: {
         Authorization: token
       } });
@@ -66,7 +66,7 @@
       const { data: user } = await axios.get(`${api}/whoami`, { headers: {
         Authorization: token
       } });
-      username = user.username;
+      id = user.id;
     })();
   }
 </script>
@@ -80,12 +80,12 @@
     {post.content}
   </div>
   <br>
-  <button class="flex items-center gap-2 bg-inherit outline-inherit border-none p-0 hover:bg-inherit" title={`loved by ${post.loves.join(', ')}`} on:click={async () => await lovePost()}>
-  {#if post.loves.includes(username)}
+  <button class="flex items-center gap-2 bg-inherit outline-inherit border-none p-0 hover:bg-inherit" title={`loved by ${post.loves.join(', ')}`} on:click={async () => { if(loggedIn) { await lovePost() } } }>
+  {#if post.loves.includes(id)}
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.6" class="w-6 h-6 fill-primary-500 stroke-primary-500">
       <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
     </svg>
-  {:else if loggedIn}
+  {:else}
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.6" class="w-6 h-6 fill-[#202026] stroke-white">
       <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
     </svg>
