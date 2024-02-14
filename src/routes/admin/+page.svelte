f3a4685552ddf94e8097564bd11689bf524b9c4f
errplane <110797323+errplanedev@users.noreply.github.com>
2024-02-13 16:32:05 -0800

    not what i want, but it works

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -6,23 +6,49 @@
 
   dayjs.extend(relativeTime);
 
+  const Cookie = {
+    set: (name: string, value: string, expiresIn: number): void => {
+      const d = new Date();
+      const t = d.getTime();
+      const et = t + expiresIn;
+      d.setTime(et);
+      document.cookie = `${name}=${value};${et};path=/`;
+    },
+  
+    delete: (name: string): void => {
+      document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/`;
+    },
+
+    get: (name: string): string | null => {
+      const cookieName = `${name}=`;
+      const decodedCookie = decodeURIComponent(document.cookie);
+      const cookieArray = decodedCookie.split(';');
+      for (let i = 0; i < cookieArray.length; i++) {
+        let cookie = cookieArray[i];
+        while (cookie.charAt(0) == ' ') {
+          cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(cookieName) == 0) {
+          return cookie.substring(cookieName.length, cookie.length);
+        }
+      }
+      return null;
+    }
+  }
+
   export let data: LayoutData;
   
   let postId: String;
   let userHandle: String;
-  let posts: Object;
-  let api;
+  let posts: Object = {};
+  let api = {};
 
   if(typeof window !== 'undefined') {
     if(!data.user.admin) {
       window.location.href = '/';
       console.log('ur not admin!!!')
     } else {
-      const a = async () => {
-        api = await axios.get('/api');
-      }
-      a();
-      console.log(api);
+      // [TODO) implement the reported posts
     }
   }
 </script>
@@ -39,9 +65,9 @@
   <input type="text" placeholder="user handle without @" bind:value={userHandle} spellcheck="false">
   <button>delete</button>
   <br>
-  <h3>reported posts (raw):</h3>
+  <h3>reported posts:</h3>
   <code>
-    {@html api}
+    wip, send a request to the api on path "/getReportedPosts" with the body (in json) token: YOUR_TOKEN_HERE
   </code>
   <br>
   <p class="font-bold text-red-600">HERE ARE SOME RULES FOR THE ADMIN PAGE:</p>
